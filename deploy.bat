2b90a5c4e27ccae68693bcbb28e5450b0f37bf2f
nepaleseintech <nepaleseintechn@gmail.com>
2022-02-22 23:19:58 -0700

    updated

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-Powershell.exe -executionpolicy remotesigned -File  C:\admin-box\app\files\idm\application\deploy_iam_adm.ps1
+Powershell.exe -executionpolicy remotesigned -File  C:\admin-box\app\files\admin\application\deploy_iam_adm.ps1
